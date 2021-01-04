2ff738d8c7741c1656774cc5f80b0551e5615129
ShaibonJpeg <47832041+ShaibonJpeg@users.noreply.github.com>
2021-01-05 00:24:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import vkquick as vq
-bot = vq.Bot.init_via_token("f757ab54278ff4eb3c6d36668b0b3a9e4f99af40e4fdaeb04294e63f50c39c1bae2540259fb7925344c3f")
+bot = vq.Bot.init_via_token("токен сюда")
 
 @bot.add_command(names=[".*(?:инсаф|insaf|556|инcaф|инсaф|инcаф).*"], use_regex_escape=False)
 async def hello_text(ctx: vq.Context):
